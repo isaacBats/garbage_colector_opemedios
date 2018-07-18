7c15e1ef29b167ef76a02fd555c24576ae5fb3ba
Isaac Daniel Batista <disa.indus@gmail.com>
2018-07-18 00:37:46 -0500

    logs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,10 @@
 
 namespace App\Console;
 
+use DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
-use DB;
+use Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,9 +26,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $filePath = 'storage/logs/schema'.date('Ymd').'.log';
         $schedule->call(function () {
             $result = DB::select("SELECT id_noticia, id_tipo_fuente FROM noticia WHERE date_format(fecha, '%Y-%m') = '2015-01'");
+            Log::info('Asi se manda un log');
+            Log::alert('Log de alerta');
+            Log::error('Log de error');
+            Log::warning('Log de warning');
+            Log::debug('Log debug');
             dd($result);
-        });
+        })
+        ->appendOutputTo($filePath);
+        // ->emailOutputTo('klonate@gmail.com');
     }
 }
