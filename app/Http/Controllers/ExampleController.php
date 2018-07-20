d05fe2c781e233b42f3de98ae42dd16978b68cda
Isaac Daniel Batista <disa.indus@gmail.com>
2018-07-19 20:32:24 -0500

    Se elimino el helper public_path

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -60,17 +60,3 @@ class ExampleController extends Controller
         return response()->json($groupBySource, 200);
     }
 }
-
-// helper
-if(!function_exists('public_path'))
-{
-    /**
-    * Return the path to public dir
-    * @param null $path
-    * @return string
-    */
-    function public_path($path=null)
-    {
-            return rtrim(app()->basePath('public/'.$path), '/');
-    }
-}
