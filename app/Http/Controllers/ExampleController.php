d8e3ae4e116c9554b93353305fd3aeb87298eaca
Isaac Daniel Batista <disa.indus@gmail.com>
2018-07-19 19:35:51 -0500

    obteniendo noticias entre un rango de fechas y agrupandolas

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
+use Log;
 
 class ExampleController extends Controller
 {
@@ -18,6 +20,38 @@ class ExampleController extends Controller
 
     public function deteleCartones(Request $req)
     {
-        return response()->json($req->all(), 200);
+        $fechaIni = $req->query('fecha_inicio');
+        $fechaFin = $req->query('fecha_fin');
+
+        Log::info("Obteniendo las noticias entre {$fechaIni} y {$fechaFin}");
+        $noticias = DB::table('noticia')
+            ->select('id_noticia', 'id_tipo_fuente as fuente') 
+            ->whereBetween(DB::raw("date_format(fecha, '%Y-%m')"), [$fechaIni, $fechaFin])
+            ->get();
+        
+        Log::info("Numero de noticias: {$noticias->count()}");
+        Log::info("Resultado: {$noticias}");
+        Log::info('Ordenando noticias por fuente...');
+        $groupBySource = array();
+        foreach ($noticias as $noticia) {
+            if(in_array($noticia->fuente, $groupBySource)) {
+                $groupBySource[$noticia->fuente][] = $noticia->id_noticia;
+            }
+            $groupBySource[$noticia->fuente][] = $noticia->id_noticia;
+        }
+
+        return $groupBySource;
+
+        // $tipos = array_values( array_unique( array_column( $products_in_collection, 'tipo' ) ) );
+        // $products = array();
+        // foreach ($products_in_collection as $item)
+        // {                
+        //     if( in_array( $item['tipo'], $tipos ) )
+        //     {
+        //         $products[ $item['tipo'].'/'.$item['_type'] ][] = $item;
+        //     }
+        // }
+
+        return response()->json($noticias, 200);
     }
 }
