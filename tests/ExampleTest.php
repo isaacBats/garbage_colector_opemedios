be8c31be6dd641699c56ea5779692e42bf8fcdf6
Isaac Daniel Batista <disa.indus@gmail.com>
2018-07-23 17:39:49 -0500

    create test for connection to the database

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,7 @@
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\DB;
 
 class ExampleTest extends TestCase
 {
@@ -17,5 +18,19 @@ class ExampleTest extends TestCase
         $this->assertEquals(
             $this->app->version(), $this->response->getContent()
         );
+    }
+
+    /**
+     * A basic test connect DB.
+     *
+     * @return json
+     */
+    public function testConnectionDatabase()
+    {
+        $fuentes = DB::table('tipo_fuente')
+            ->select()
+            ->get();
+
+        $this->assertEquals($fuentes->count(), 5);
     }
 }
